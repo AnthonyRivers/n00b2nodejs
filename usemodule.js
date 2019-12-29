9453d1df0774abbd68a3cf742c4b56d2286cd779
Antonio Rios <antonio.rios.jr@gmail.com>
2019-12-29 12:08:41 -0700

    Added code to show how EventEmitter works.

--- usemodule.js
+++ usemodule.js
@@ -1,4 +1,5 @@
-const collectAnswers = require("./lib/collectAnswers");
+const collectAnswers = require("./lib/improveWithEventEmitter");
+//const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
 	"What is your name?",
@@ -6,8 +7,16 @@ const questions = [
 	"What are you going to do with node.js?"
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions, answers => {
 	console.log("Thank you for your answers.");
 	console.log(answers);
 	process.exit();
 });
+
+answerEvents.on("answer", answer => console.log(`question answered: ${answer}`));
+answerEvents.on("complete", answers => {
+	console.log("Thank you for your answers.");
+	console.log(answers);
+});
+
+answerEvents.on("complete", () => process.exit());
